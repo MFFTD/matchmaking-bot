de9c0a562ff3fc7f4710b930baae0ca47fa4bf89
MFFTD <156596089+MFFTD@users.noreply.github.com>
2024-02-28 15:10:16 +0200

    Add files via upload

---
+++ utils/db.py
@@ -0,0 +1,34 @@
+import aiosqlite
+import asyncio
+
+class AsyncSQLiteDB:
+    def __init__(self, db_path='mf5s.db'):
+        self.db_path = db_path
+
+    async def connect(self):
+        try:
+            connection = await aiosqlite.connect(self.db_path)
+            return connection
+        except aiosqlite.Error as e:
+            print(f"Error connecting to the database: {e}")
+
+    async def close(self, connection):
+        try:
+            if connection:
+                await connection.close()
+        except aiosqlite.Error as e:
+            print(f"Error closing the database connection: {e}")
+
+    async def execute_query(self, query, params=None, fetchall=False):
+        connection = await self.connect()
+        try:
+            async with connection.cursor() as cursor:
+                await cursor.execute(query, params)
+                if fetchall:
+                    return await cursor.fetchall()
+                else:
+                    await connection.commit()
+        except aiosqlite.Error as e:
+            print(f"Error executing query: {e}")
+        finally:
+            await self.close(connection)
